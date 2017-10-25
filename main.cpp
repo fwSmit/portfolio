c1eb6ed6aea06dc9b5e028e4953ebfa148f52639
fwsmit <fw.smit01@gmail.com>
2017-10-25 12:09:23 +0200

    added all dependencies

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <TGUI/TGUI.hpp>
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+void buttonPressed(tgui::Gui& gui);
+
+int main()
+{
+    sf::RenderWindow window{{800, 600}, "Window"};
+    tgui::Gui gui{window}; // Create the gui and attach it to the window
+	tgui::Theme::Ptr theme = tgui::Theme::create("/home/friso/tgui-git/src/TGUI/widgets/Black.txt");
+
+    tgui::Button::Ptr button = theme->load("button");
+	button->setText("start");
+	button->setSize(sf::Vector2f(200, 50));
+	button->connect("pressed", buttonPressed, std::ref(gui));
+    //tgui::Button::Ptr button2 = tgui::Button::create("second");
+    button->setPosition(400-button->getSize().x/2, 200);
+    
+    gui.add(button, "button");
+    //gui.add(button2, "button2");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            gui.handleEvent(event); // Pass the event to the widgets
+        }
+
+        window.clear();
+        gui.draw(); // Draw all widgets
+        window.display();
+    }
+}
+
+
+void buttonPressed(tgui::Gui& gui){
+	cout << "button pressed" << endl;
+	const tgui::Widget::Ptr button = gui.get("button");
+	button->hide();
+
+	
+	
+}
